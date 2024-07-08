ab90ef3b8d792bfccf8c4c40b3dc40bc19e82d39
Kichang Kim <kichangkim@hotmail.com>
2024-07-08 21:38:52 +0900

    Fix : headers does not need anymore.

--- deepdanbooru/commands/download_tags.py
+++ deepdanbooru/commands/download_tags.py
@@ -34,7 +34,6 @@ def download_category_tags(
     }
 
     request_url = "https://danbooru.donmai.us/tags.json"
-    headers={'User-Agent': 'Mozilla/5.0'}
 
     tags = set()
 
@@ -42,8 +41,8 @@ def download_category_tags(
         response = requests.get(
             request_url,
             params=parameters,
-            headers=headers,
         )
+
         response_json = response.json()
 
         response_tags = [
